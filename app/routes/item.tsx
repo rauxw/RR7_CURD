eb0fe274088f2109c946f3a4095d0cd7a643dec8
hardik ketan raut <hardikketanraut@hardiks-MacBook-Air.local>
2025-04-12 10:00:33 +0530

    added edit and delete methods

--- app/routes/item.tsx
+++ app/routes/item.tsx
@@ -1,3 +1,97 @@
-export default function Item() {
-  return <div>Items</div>;
+import { supabase } from "~/supabase-client";
+import type { Route } from "./+types/item";
+import { Form, redirect, type ActionFunctionArgs } from "react-router";
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const { id } = params;
+  if (!id) {
+    return { error: "No Item found" };
+  }
+
+  const { data, error } = await supabase
+    .from("items")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    return { error: error.message };
+  }
+  return { item: data };
+}
+
+export async function action({ request, params }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const title = formData.get("title") as String;
+  const description = formData.get("description") as String;
+  const intent = formData.get("intent");
+
+  if (intent === "delete") {
+    const { error } = await supabase.from("items").delete().eq("id", params.id);
+    if (error) {
+      return { error: error.message };
+    }
+    return redirect("/");
+  } else if (intent === "update") {
+    const { error } = await supabase
+      .from("items")
+      .update({ title, description })
+      .eq("id", params.id);
+    if (error) {
+      return { error: error.message };
+    }
+    return redirect("/");
+  }
+  return {};
+}
+
+export default function Item({ loaderData }: Route.ComponentProps) {
+  const { item } = loaderData;
+  return (
+    <div>
+      <h2>Edit Items</h2>
+      <Form method="POST">
+        {" "}
+        <div className="flex flex-col items-center mb-5">
+          <label className="">Title</label>
+          <input
+            className="p-2 mt-1 mb-1 border rounded-md text-center"
+            placeholder="Enter title for product"
+            type="text"
+            name="title"
+            defaultValue={item.title}
+            required
+          />
+        </div>
+        <div className="flex flex-col items-center mb-5">
+          <label className="">Description </label>
+          <textarea
+            className="p-3 m-3 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-center"
+            placeholder="Enter Description for Product"
+            name="description"
+            defaultValue={item.description}
+            required
+          />
+        </div>
+        <div className="flex justify-center w-full">
+          <button
+            className="bg-blue-500 text-blue-100 hover:bg-blue-700 hover:text-black font-bold py-2 px-4 rounded transition duration-300 m-3"
+            type="submit"
+            value="update"
+            name="intent"
+          >
+            Update Item
+          </button>
+          <button
+            className="bg-blue-500 text-blue-100 hover:bg-blue-700 hover:text-black font-bold py-2 px-4 rounded transition duration-300 m-3"
+            type="submit"
+            value="delete"
+            name="intent"
+          >
+            Delete Item
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
 }
